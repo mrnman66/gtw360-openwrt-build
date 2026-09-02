c13e500d19f0f08b380dafa214a75e4308946a82
mrnman66 <106779214+mrnman66@users.noreply.github.com>
2026-09-02 15:33:26 +0300

    Add files via upload

---
+++ custom/target/linux/imx/cortexa7/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,107 @@
+. /lib/imx.sh
+
+RAMFS_COPY_BIN='blkid jffs2reset'
+
+enable_image_metadata_check() {
+	case "$(board_name)" in
+	technexion,imx7d-pico-pi)
+		REQUIRE_IMAGE_METADATA=1
+		;;
+	esac
+}
+enable_image_metadata_check
+
+platform_check_image() {
+	local board=$(board_name)
+
+	case "$board" in
+	geniatech,gtw360 )
+		local platform_dir_name=$(echo $board | sed 's/,/_/g')
+		nand_do_platform_check $platform_dir_name $1
+		return $?;
+		;;
+	technexion,imx7d-pico-pi)
+		return 0
+		;;
+	esac
+
+	echo "Sysupgrade is not yet supported on $board."
+	return 1
+}
+
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	geniatech,gtw360 )
+	  platform_do_flash_fit "$1" "$board" "fitImageA" "$(platform_get_rootfs)"
+		;;
+	technexion,imx7d-pico-pi)
+		imx_sdcard_do_upgrade "$1"
+		;;
+	esac
+}
+
+platform_get_rootfs() {
+	local rootfsdev
+
+	if read cmdline < /proc/cmdline; then
+		case "$cmdline" in
+			*root=*)
+				rootfsdev="${cmdline##*root=}"
+				rootfsdev="${rootfsdev%% *}"
+			;;
+		esac
+
+		echo "${rootfsdev}"
+	fi
+}
+
+platform_do_flash_fit() {
+	local tar_file=$1
+	local board=$2
+	local kernel=$3
+	local rootfs=$4
+
+	local board_dir=$(tar tf "$tar_file" | grep -m 1 '^sysupgrade-.*/$')
+	board_dir=${board_dir%/}
+	[ -n "$board_dir" ] || return 1
+
+	mkdir -p /boot
+
+	if [ $board = "geniatech,gtw360" ]; then
+		# mmcblk1p1 (fat) contains all FIT images for the Gateway
+		mount -t vfat /dev/mmcblk1p1 /boot
+
+		echo "flashing Kernel to /boot/$kernel (/dev/mmblk1p1)"
+		tar -Oxf $tar_file "$board_dir/kernel" > /boot/$kernel
+	fi
+
+	echo "flashing rootfs to ${rootfs}"
+	# erase the rootfs partition to clean up the old overlay data in f2fs after the squashfs
+	mtd erase "${rootfs}"
+	tar xf $tar_file $board_dir/root -O | dd of="${rootfs}" bs=4096
+
+	sync
+	umount /boot
+}
+
+platform_copy_config() {
+	local board=$(board_name)
+
+	case "$board" in
+	technexion,imx7d-pico-pi)
+		imx_sdcard_copy_config
+		;;
+	esac
+}
+
+platform_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	technexion,imx7d-pico-pi)
+		imx_sdcard_pre_upgrade
+		;;
+	esac
+}
---
+++ custom/target/linux/imx/image/cortexa7.mk
@@ -0,0 +1,48 @@
+DEVICE_VARS += UBOOT
+
+include common.mk
+
+define Device/Default
+  PROFILES := Default
+  FILESYSTEMS := squashfs ext4
+  KERNEL_INSTALL := 1
+  KERNEL_SUFFIX := -uImage
+  KERNEL_NAME := zImage
+  KERNEL := kernel-bin | uImage none
+  KERNEL_LOADADDR := 0x80008000
+  DTS_DIR := $(DTS_DIR)/nxp/imx
+  IMAGES :=
+endef
+
+define Device/geniatech_gtw360
+  DEVICE_VENDOR := Geniatech
+  DEVICE_MODEL := GTW360
+  DEVICE_DTS := imx6ull-gtw360
+  IMAGES := sysupgrade.bin dtb rootfs.bin
+  IMAGE/dtb := install-dtb
+  IMAGE/rootfs.bin := append-rootfs
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  KERNEL := kernel-bin | fit none $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_SUFFIX := -zImage
+  DEVICE_PACKAGES := kmod-brcmfmac kmod-button-hotplug kmod-input-gpio-keys \
+    kmod-leds-gpio kmod-pps-gpio kmod-fs-vfat kmod-phy-micrel \
+    wpa-supplicant ca-certificates hostapd \
+    kmod-bluetooth bluez-daemon bluez-libs bluez-utils
+endef
+TARGET_DEVICES += geniatech_gtw360
+
+define Device/technexion_imx7d-pico-pi
+  DEVICE_VENDOR := TechNexion
+  DEVICE_MODEL := PICO-PI-IMX7D
+  UBOOT := pico-pi-imx7d
+  DEVICE_DTS := imx7d-pico-pi
+  DEVICE_PACKAGES := kmod-sound-core kmod-sound-soc-imx kmod-sound-soc-imx-sgtl5000 \
+	kmod-can kmod-can-flexcan kmod-can-raw kmod-leds-gpio \
+	kmod-input-touchscreen-edt-ft5x06 kmod-usb-hid kmod-btsdio \
+	kmod-brcmfmac brcmfmac-firmware-4339-sdio cypress-nvram-4339-sdio
+  FILESYSTEMS := squashfs
+  IMAGES := combined.bin sysupgrade.bin
+  IMAGE/combined.bin := append-rootfs | pad-extra 128k | imx-sdcard-raw-uboot
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += technexion_imx7d-pico-pi
